76e8ab938d1d8ce6e8201f0dd10da82ab8fdc9c2
Michał Kaczanowicz <m.kaczanowicz@vp.pl>
2018-04-15 04:35:27 +0200

    Add importing initial data for model from csv file

---
+++ GaussApp/gravity/management/commands/import_csv.py
@@ -0,0 +1,34 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+
+from gravity.models import Company
+
+SILENT, NORMAL, VERBOSE, VERY_VERBOSE = 0,1,2,3
+
+class Command(BaseCommand):
+    help = "Imports companies data to database from CSV file in format: name, longtitude, latitude, number_of_opinions, average_opinion"
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_path')
+
+    def handle(self, *args, **options):
+        verbosity = int(options.get("verbosity", NORMAL))
+
+        file_path = options['file_path']
+
+        with open(file_path) as f:
+            reader = csv.reader(f)
+            for name, longtitude, latitude, number_of_opinions, average_opinion in reader:
+                company, created = Company.objects.get_or_create(
+                    name = name,
+                    x = longtitude,
+                    y = latitude,
+                    opinions = number_of_opinions,
+                    average_opinion = average_opinion,
+                )
+                if verbosity >= NORMAL:
+                    print(" - " + name)
+        
+        if verbosity >= NORMAL:
+            print("=== Companies imported ===")
