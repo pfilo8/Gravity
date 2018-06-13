ae809e2f1bc4b21e36496a95a189ac29e38a93f1
Michał Kaczanowicz <m.kaczanowicz@vp.pl>
2018-06-13 14:14:18 +0200

    Remove trailing slashes from urls

--- GaussApp/gravity/urls.py
+++ GaussApp/gravity/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.api, name='api'),
-    path('rcmd/', views.rcmd, name='rcmd'),
+    path('api', views.api, name='api'),
+    path('rcmd', views.rcmd, name='rcmd'),
     path('', views.index, name='index'),
 ]
